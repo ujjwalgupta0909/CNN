3175fac456cce83843b525f117e41eaf9951e774
ujjwalgupta0909 <44123783+ujjwalgupta0909@users.noreply.github.com>
2019-03-10 15:50:27 +0530

    Add files via upload

---
+++ CNN.py
@@ -0,0 +1,65 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Jan 27 10:38:30 2019
+
+@author: hp
+"""
+
+#Import the keras libraries and packages
+
+#to initiate the neural network
+from keras.models import Sequential
+
+from keras.layers import Convolution2D
+from keras.layers import MaxPooling2D
+from keras.layers import Flatten
+from keras.layers import Dense
+
+#initialising the CNN
+classifier=Sequential()
+
+#convolution
+classifier.add(Convolution2D(32,3,3,input_shape=(64,64,3),activation='relu'))
+
+#pooling
+classifier.add(MaxPooling2D(pool_size=(2,2)))
+
+#flattening
+classifier.add(Flatten())
+
+#full connection
+classifier.add(Dense(output_dim=128,activation='relu'))
+classifier.add(Dense(output_dim=1,activation='sigmoid'))
+
+#compile the cnn
+classifier.compile(optimizer='adam',loss='binary_crossentropy',metrics=['accuracy'])
+
+#fitting the cnn to images
+from keras.preprocessing.image import ImageDataGenerator
+
+train_datagen = ImageDataGenerator(
+        rescale=1./255,
+        shear_range=0.2,
+        zoom_range=0.2,
+        horizontal_flip=True)
+
+test_datagen = ImageDataGenerator(rescale=1./255)
+
+training_set = train_datagen.flow_from_directory(
+        'dataset/training_set',
+        target_size=(64,64),
+        batch_size=32,
+        class_mode='binary')
+
+test_set = test_datagen.flow_from_directory(
+        'dataset/test_set',
+        target_size=(64,64),
+        batch_size=32,
+        class_mode='binary')
+
+classifier.fit_generator(
+        training_set,
+        steps_per_epoch=8000,
+        epochs=25,
+        validation_data=test_set,
+        validation_steps=2000)
